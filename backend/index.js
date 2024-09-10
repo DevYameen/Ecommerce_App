835d7edcc93a8e58ab61d8236592e3c02f95f06f
DevYameen <yammirza793@gmail.com>
2024-09-10 18:47:11 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-const connectDB = require('./config/db');
-const router = require('./routes');
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
 
-const app = express();
 
-// CORS middleware configuration to allow all origins temporarily
+const app = express()
 app.use(cors({
-  origin: '*',  // Allow all origins temporarily
-  credentials: true
-}));
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+app.use(express.json())
+app.use(cookieParser())
 
-app.use(express.json());
-app.use(cookieParser());
+app.use("/api",router)
 
-app.use("/api", router);
-
-const PORT = process.env.PORT || 8080;
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Connected to DB");
-    console.log(`Server is running on port ${PORT}`);
-  });
-});
+const PORT = 8080 || process.env.PORT
 
 
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
+    })
+})
